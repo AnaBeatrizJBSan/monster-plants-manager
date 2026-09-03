fa72677d72286cbd8a6a1efda808a8b0ac217112
AnaBeatrizJBSan <anabeatrizauguto4@gmail.com>
2026-09-03 09:25:39 -0300

    feat: add typings

---
+++ src/types/env.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    HABBO_WIRED_READ_KEY?: string;
+    HABBO_WIRED_WRITE_KEY?: string;
+    EXPO_PUBLIC_HABBO_HOTEL?: string;
+    EXPO_PUBLIC_HABBO_ROOM_ID?: string;
+    EXPO_PUBLIC_MONSTER_PLANTS_ENDPOINT?: string;
+  }
+}
