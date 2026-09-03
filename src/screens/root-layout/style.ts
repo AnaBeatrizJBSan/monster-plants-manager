d6ac138ce5eb4ad6dfc8cec48c8836e11ef0c8cb
AnaBeatrizJBSan <anabeatrizauguto4@gmail.com>
2026-09-03 09:26:57 -0300

    feat: add styling

---
+++ src/screens/root-layout/style.ts
@@ -0,0 +1,13 @@
+export const screenOptions = {
+  headerStyle: {
+    backgroundColor: "#f0c24d",
+  },
+  headerTintColor: "#472400",
+  headerTitleStyle: {
+    fontWeight: "900" as const,
+  },
+  headerShadowVisible: false,
+  contentStyle: {
+    backgroundColor: "#f9e3a3",
+  },
+};
